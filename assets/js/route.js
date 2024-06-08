0e66ee0f89cac0d967d25e8d965a9d4e8396289b
duranangie <duranangie>
2024-06-07 23:00:51 -0500

    feat: :sparkles: se llama el app.js

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { updateWeather, error404 } from "./app.js";
+
 const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474" //London
 
 const currentLocation = function(){
@@ -32,7 +33,7 @@ const checkHash = function(){
 
     const requestURL = window.location.hash.slice(1);
 
-    const [route, query] = requestURL.includes ? requestURL.split("?"):[requestURL];
+    const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
 
     routes.get(route) ? routes.get(route)(query) : error404();
 
