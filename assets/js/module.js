83661aace6f3225f1d6be509689d38e0351f2e18
duranangie <duranangie>
2024-06-07 21:01:03 -0500

    feat: :sparkles: creacion de js donde indicamos el manejo de fecha hora y dia de la semana

---
+++ assets/js/module.js
@@ -0,0 +1,116 @@
+'use strict'
+
+export const weekDayNames = [
+
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+/**
+ * 
+ * @param {number} dateUnix unix date in second
+ * @param {number} timezone Timezone shift from utc in seconds 
+ * @returns {string} Date String formate "sunday 20 , jan"
+ */
+
+export const getDate = function (dateUnix, timezone) {
+    const date = new Date((dateUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames[date.getUTCMonth()];
+
+
+    return `${weekDayName} ${date.getUTCDate()},${monthName}`;
+}
+
+/**
+ * 
+ * @param {number} timeUnix  unix date in seconds
+ * @param {number} timezone  timezone shift from UTC
+ * @returns {string} time string . formate "hh:mm am/pm"
+ */
+
+export const getTime = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12}:${minutes} ${period}`;
+}
+
+
+
+
+
+/**
+ * 
+ * @param {number} timeUnix  unix date in seconds
+ * @param {number} timezone  timezone shift from UTC
+ * @returns {string} time string . formate "hh am/pm"
+ */
+
+export const getHours = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+/**
+ * 
+ * @param {number} mps 
+ * @returns {number}
+ */
+
+
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk "
+    },
+    2: {
+        level: "Fair",
+        message: "Air quality is acceptable; however, for some pollutantes there may be a moderate health concern for very small number of people who are unusually sensitive to air pollution. "
+    },
+    3: {
+        level: "Moderante",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected "
+    },
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience healt effects; members of sensitive groups may experience more serious health affects"
+    },
+    5: {
+        level: "Very Poor ",
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected"
+    }
+
+
+
+}
