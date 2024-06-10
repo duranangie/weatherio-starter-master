91843a0edf8d199239a6cc051f334c00950d0b90
duranangie <duranangie>
2024-06-10 01:56:43 -0500

    feat: :sparkles: modificar codigo para el api

--- assets/js/route.js
+++ assets/js/route.js
@@ -2,25 +2,16 @@
 
 import { updateWeather, error404 } from "./app.js";
 
-const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474" //London
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"; // Londres
 
-const currentLocation = function(){
-
-    window.navigator.geolocation.getCurrentPosition(res =>{
-        const { latitude, longitude} = res.coords;
-
+const currentLocation = function() {
+    window.navigator.geolocation.getCurrentPosition(res => {
+        const { latitude, longitude } = res.coords;
         updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-    
-
-    }, err=>{
-        window.location.hash=defaultLocation;
-    })
-
-}
-/**
- * 
- * @param {string} query 
- */
+    }, err => {
+        window.location.hash = defaultLocation;
+    });
+};
 
 const searchedLocation = query => updateWeather(...query.split("&"));
 
@@ -29,23 +20,18 @@ const routes = new Map([
     ["/weather", searchedLocation]
 ]);
 
-const checkHash = function(){
-
+const checkHash = function() {
     const requestURL = window.location.hash.slice(1);
-
-    const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
-
+    const [route, query] = requestURL.includes("?") ? requestURL.split("?") : [requestURL];
     routes.get(route) ? routes.get(route)(query) : error404();
+};
 
-}
+window.addEventListener("hashchange", checkHash);
 
-
-window.addEventListener("hashchance", checkHash);
-
-window.addEventListener("load",function(){
-    if(!window.location.hash){
+window.addEventListener("load", function() {
+    if (!window.location.hash) {
         window.location.hash = "#/current-location";
-    }else{
+    } else {
         checkHash();
     }
-})
+});
